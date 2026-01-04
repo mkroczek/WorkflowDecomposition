b0e1f0231796ba72ef6bcb9875851a491af7f247
mkroczek <1marcin100@gmail.com>
2026-01-04 22:54:40 +0100

    Resolve problem with sympy

--- decomposition/qhyper/problem.py
+++ decomposition/qhyper/problem.py
@@ -1,3 +1,5 @@
+from QHyper.constraint import Constraint, Operator, MethodsForInequalities
+from QHyper.polynomial import Polynomial
 from QHyper.problems.workflow_scheduling import WorkflowSchedulingOneHot
 
 
@@ -21,3 +23,48 @@ def decorate(problem: WorkflowSchedulingOneHot):
     problem.calculate_solution_cost = calculate_solution_cost
     problem.calculate_solution_timespan = calculate_solution_timespan
     return problem
+
+
+class WorkflowSchedulingOneHotEnhanced(WorkflowSchedulingOneHot):
+    def _set_objective_function(self):
+        expression_dict = {}
+        cost_per_variable = zip(self.variables, self.workflow.cost_matrix.to_numpy().flatten())
+        cost_per_variable = sorted(cost_per_variable, key=lambda x: str(x[0]))
+        for variable, cost in cost_per_variable:
+            cost_str = "{:.15g}".format(cost)
+            expression_dict[tuple([str(variable)])] = float(cost_str)
+        self.objective_function: Polynomial = Polynomial(expression_dict)
+
+    def _set_constraints(self):
+        self.constraints: list[Constraint] = []
+
+        # machine assignment constraint
+        for task_id in range(len(self.workflow.time_matrix.index)):
+            machine_assignment_expr_dict = {}
+            for machine_id in range(len(self.workflow.time_matrix.columns)):
+                variable = self.variables[machine_id + task_id * len(self.workflow.time_matrix.columns)]
+                machine_assignment_expr_dict[tuple([str(variable)])] = 1
+            self.constraints.append(Constraint(Polynomial(machine_assignment_expr_dict), 1, Operator.EQ))
+
+        # deadline constraint
+        for path in self.workflow.paths:
+            path_expr_dict = {}
+            for task_id, task_name in enumerate(self.workflow.time_matrix.index):
+                for machine_id, machine_name in enumerate(
+                        self.workflow.time_matrix.columns
+                ):
+                    if task_name in path:
+                        time = self.workflow.time_matrix[machine_name][task_name]
+                        variable = self.variables[machine_id + task_id * len(self.workflow.time_matrix.columns)]
+                        time_str = "{:.15g}".format(time)
+                        path_expr_dict[tuple([str(variable)])] = float(time_str)
+
+            # todo add constraints unbalanced penalization
+            self.constraints.append(
+                Constraint(
+                    Polynomial(path_expr_dict),
+                    self.workflow.deadline,
+                    Operator.LE,
+                    MethodsForInequalities.UNBALANCED_PENALIZATION,
+                )
+            )
